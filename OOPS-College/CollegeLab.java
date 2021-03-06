536cf219bfc9b7ed5ecfd7c18d7acf89687b286e
Sangeetha <ssangeethapugal@gmail.com>
2021-03-06 19:23:59 +0530

    Revert ""
    This reverts commit 0b216a1cccfe0ffb8bba17b4fc00562c90aaaa33.

--- OOPS-College/CollegeLab.java
+++
@@ -1,6 +0,0 @@
-package javafundamental.CollegeApplication;
-
-public abstract interface CollegeLab {
-    public void labFacility();
-    public void musicFacility();
-}
--- OOPS-College/CollegeLibrary.java
+++
@@ -1,8 +0,0 @@
-package javafundamental.CollegeApplication;
-
-public abstract interface CollegeLibrary {
-    public void bookDetails(String author,String name, int isbn,boolean isRented);
-    public void furnitureDetails(String type,String material);
-
-
-}
